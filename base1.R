bbcf656735a5d4c42e369fc464369c46ba433c62
Danix43 <daninitu39@gmail.com>
2025-05-12 13:43:02 +0300

    Update

--- base1.R
+++ base1.R
@@ -14,7 +14,7 @@ jarque.bera.test(reziduale)
 durbinWatsonTest(model)
 
 # 6. Testul heteroscedasticității - Goldfeld-Quandt
-# install.packages("lmtest")
+#install.packages("lmtest")
 #library(lmtest)
 gqtest(model)
 
@@ -22,11 +22,17 @@ gqtest(model)
 vif(model)
 
 # 8. Prognoza pentru o nouă observație
-nou <- data.frame(data$cost = 200, data$clicks = 500)
+nou <- data.frame(cost = c(2.00, 2.10, 2.20, 2.30, 2.50), clicks = c(500, 550, 600, 650, 700))
+
+# predict pentru revenue
+# pe baza modelului si al valorilor introduce, functia predict genereaza valorile
 predict(model, newdata = nou, interval = "confidence")
+# predict(model, newdata = nou, interval = "prediction")
+
+
 
 qqnorm(residuals(model)); qqline(residuals(model))
 
 par(mfrow = c(2, 2))
-plot(model)
+plot(model) 
 par(mfrow = c(1, 1))
