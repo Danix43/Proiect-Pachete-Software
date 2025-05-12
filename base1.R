abc362ca0c32023c5c7bf81925c161a104c6ae96
Danix43 <daninitu39@gmail.com>
2025-05-12 12:34:53 +0300

    Update

--- base1.R
+++ base1.R
@@ -1,4 +1,32 @@
 data <- online_advertising_performance_data
 
-model <- lm(data$revenue ~ data$clicks + data$cost, data = data)
+# model liniar
+model <- lm(revenue ~ clicks + cost, data = data)
 summary(model)
+
+# install.packages("tseries")
+# library(tseries)
+reziduale <- residuals(model)
+jarque.bera.test(reziduale)
+
+# install.packages("car") 
+# library(car)
+durbinWatsonTest(model)
+
+# 6. Testul heteroscedasticității - Goldfeld-Quandt
+# install.packages("lmtest")
+#library(lmtest)
+gqtest(model)
+
+# 7. Testarea multicolinearității - VIF
+vif(model)
+
+# 8. Prognoza pentru o nouă observație
+nou <- data.frame(data$cost = 200, data$clicks = 500)
+predict(model, newdata = nou, interval = "confidence")
+
+qqnorm(residuals(model)); qqline(residuals(model))
+
+par(mfrow = c(2, 2))
+plot(model)
+par(mfrow = c(1, 1))
